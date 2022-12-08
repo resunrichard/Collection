4dc769cdec1c4eaae6cb96ed29e815de12f7da4e
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-12-08 21:45:45 +0530

    Add files via upload

---
+++ StreamDemo1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.Collectors;
+public class StreamDemo1 {
+public static void main(String args[]){
+ArrayList<Integer> marks=new ArrayList<Integer>();
+marks.add(70);
+marks.add(55);
+marks.add(25);
+System.out.println(marks);
+//filter the pass marks
+List<Integer> passmark=marks.stream().filter(m->m>=35).collect(Collectors.toList());
+System.out.println(passmark); }}
